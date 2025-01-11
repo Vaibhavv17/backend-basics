6106b0bce6b8f35fdd57c4ececa5712e93cb78ac
VaibhavvM <mvaibhavv17@gmail.com>
2025-01-11 23:06:29 +0530

    basic file upload

--- src/db/index.js
+++ src/db/index.js
@@ -1,6 +1,9 @@
 import mongoose from "mongoose";
 import { DB_NAME } from "../constants.js";
 
+//middleware is like jane se pehle mujhse milke jana 
+
+
 const connectDB = async () => {
   try {
     const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
